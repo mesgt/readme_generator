3f4fb9b7337743b7e38268612dc8607bf34693bb
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-25 23:17:41 -0600

    Restructured questions to be in inquirer format. Added readme layout to generateMarkdown.js. Questions display and responses are showing up in console.log.

--- index.js
+++ index.js
@@ -1,95 +1,92 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+// const generate = require("./utils/generateMarkdown");
+// const { lookup } = require("dns");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your project title?",
-    "Enter a short description of your project (what, why and how):",
-    "Enter link to the deployed application:",
-    "Would you like to include a table of contents? (yes/no)", //add condition for if 1)yes- include a list with links to these sections; 2)no is selected, do not display this heading.
-    "What are (if any) installation requirements for your project?",
-    "Provide instruction and examples of use:",
-    "Would you like to add screenshots? (yes/no)", //add condition for if 1)yes- display screenshot instructions 2)no is selected- don't display instructions.
-    "List collaborators or third-party attribution (if any):",
-    "List the license:",
-    "List badges you would like displayed:",
-    "Would you like to have others contribute to your code?",
-    "Would you like to reference any tests for this project?"
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Enter a short description of your project (including what, why and how):",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Enter link to the deployed application:",
+        name: "link",
+    },
+    {
+        type: "input",
+        message: "Would you like to include a table of contents? (yes/no)",
+        name: "tableContents",
+    },
+    {
+        type: "input",
+        message: "What are (if any) installation requirements for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Provide instruction and examples of use:",
+        name: "instructions",
+    },
+    {
+        type: "input",
+        message: "Would you like to add screenshots? (yes/no)",
+        name: "screenShots",
+    },
+    {
+        type: "input",
+        message: "List collaborators or third-party attribution (if any):",
+        name: "collaborators",
+    },
+    {
+        type: "input",
+        message: "List the license:",
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "List badges you would like displayed:",
+        name: "badges",
+    },
+    {
+        type: "input",
+        message: "List guidelines for how you would like to have others contribute to your code:",
+        name: "moreCollab",
+    },
+    {
+        type: "input",
+        message: "Would you like to reference any tests for this project? (yes/no)",
+        name: "tests",
+    },
 ];
 
 inquirer
-    .prompt([
-        {
-            type: "input",
-            message: questions[0],
-            name: "title",
-        },
-        {
-            type: "input",
-            message: questions[1],
-            name: "description",
-        },
-        {
-            type: "input",
-            message: questions[2],
-            name: "link",
-        },
-        {
-            type: "input",
-            message: questions[3],
-            name: "tableContents",
-        },
-        {
-            type: "input",
-            message: questions[4],
-            name: "installation",
-        },
-        {
-            type: "input",
-            message: questions[5],
-            name: "instructions",
-        },
-        {
-            type: "input",
-            message: questions[6],
-            name: "screenShots",
-        },
-        {
-            type: "input",
-            message: questions[7],
-            name: "collaborators",
-        },
-        {
-            type: "input",
-            message: questions[8],
-            name: "license",
-        },
-        {
-            type: "input",
-            message: questions[9],
-            name: "badges",
-        },
-        {
-            type: "input",
-            message: questions[10],
-            name: "moreCollab",
-        },
-        {
-            type: "input",
-            message: questions[11],
-            name: "tests",
-        },
-    ])
+    .prompt(questions).then(response => console.log(response));
+        
+
+        //     }
+        // }
+    // });
+        // console.log(response));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, 
-        err => err ? console.error(error) : console.log("Success! Your ReadMe.md is being generated."));
-}
+// function writeToFile(readMe) {
+//     fs.writeFile("readMe.md", html, 
+//         err => err ? console.error(err) : 
+//         console.log("Success! Your ReadMe.md is being generated."));
+// }
 
+// writeToFile()
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  //switch statement. link it
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +15,42 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  `# ${response.title}
+         
+  ## Description 
+  ${response.description}
+  
+  ## Links 
+  Published site: ${response.link}` 
+ if (tableContents==="no") {
+     console.log("You chose to not include a Table of Contents");
+ }
+ else {
+     `## Table of Contents
+
+     ## Installation
+     ${installation}
+
+     ## Usage
+     ${instructions}`
+     if (screenShots==="no") {
+         console.log("You chose to not include screenshots");
+     }
+     else {
+         `**Screenshots: <img src="assets/image/fileName.png" alt="#" width="300" /> 
+         
+         ## Credits
+         ${collaborators}
+         
+         ## License
+         ${license}
+         
+         ## Badges
+         ${badges}
+         
+         ## Contributing
+         ${moreCollab}
+         
 
 `;
 }
